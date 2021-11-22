40122406a14bcd5fc333b86894163c3846152702
juanmasorca <42307857+juanmasorca@users.noreply.github.com>
2021-11-21 20:59:20 -0500

    código con comentarios

--- main.c
+++ main.c
@@ -1,20 +1,24 @@
 /* ========================================
  *
- * Copyright YOUR COMPANY, THE YEAR
+ * Copyright Universidad Distrital Francisco José de Caldas, 2021
  * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
+ * Ingeniería en Control y Automatización 
+Integrantes
+- Juan David Mazorca Espitia
+- César Camilo Alonso Castro
+- Luis Fernando Garcia 
  *
  * ========================================
 */
+
 #include "project.h"
-#include <stdio.h>
+#include <stdio.h> // libreria para manejar los sprintf 
+
+// Declaración de variables
 int dere = 0;
 int izq = 0;
 
-
+// Interrupciones
 CY_ISR(DerechaInt){
     if(dere == 0){
         izq = 1;
@@ -29,38 +33,35 @@ int main(void)
     int32 output;
     char8 str[12];
     float ADCVoltaje;
-    CyGlobalIntEnable; /* Enable global interrupts. */
+    CyGlobalIntEnable; // Habilita las interrupciones globales
     
     
-    isrDerecha_StartEx(DerechaInt);
-    Clock_1_Start();
-    PWM_Start();
+    isrDerecha_StartEx(DerechaInt); // Inicia las interrupciones
+    Clock_1_Start(); // Inicia el reloj
+    PWM_Start(); // Inicial el PWM
+    I2C_Start(); // Inicial el modulo I2C
+    LCD_I2C_start(); // Inicia el modulo de la LCD con I2C
     
-    I2C_Start();
-    LCD_I2C_start();
-    LCD_I2C_setCursor(0,1);
+    LCD_I2C_setCursor(0,1); // Columna x Fila
     LCD_I2C_print("Voltaje");
 
-    ADC_Start(); //inicialización de el bloque LCD
+    ADC_Start(); //inicialización de el bloque ADC
     ADC_StartConvert();
     ADC_IsEndConversion(ADC_WAIT_FOR_RESULT);
     
     for(;;)
     {
         output=ADC_GetResult32();
-        ADCVoltaje=(5.000000/1048576)*output;
+        ADCVoltaje=(5.000000/1048576)*output; // (5v*2^20 bits)/valor potenciometro
         LCD_I2C_setCursor(9,1);
-        sprintf(str,"%.2f",ADCVoltaje);
+        sprintf(str,"%.2f",ADCVoltaje); // Float a String
         LCD_I2C_print(str);
-        if(izq ==1){
+        if(izq ==1){ // Giro del motor a la izquierda
            
             LED_Write(0);
             
-            
-            dere=1;
-        
-        
-        
+            dere=1; // estado del motor
+       
             if(ADCVoltaje < 0.5){
                 PWM_WriteCompare1(25);
                 PWM_WriteCompare2(0);
@@ -121,13 +122,14 @@ int main(void)
                 LCD_I2C_setCursor(0,0);
                 LCD_I2C_print("Velocidad: 100%");
                 
-            }
+            }            
         }
-        if(izq ==0){
+        if(izq ==0){ // Giro del motor a la derecha
             
            LED_Write(1); 
         
            dere=0;
+
         
            if(ADCVoltaje < 0.5){
                 PWM_WriteCompare2(25);
